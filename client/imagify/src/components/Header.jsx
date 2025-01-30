b918028036a937e96711816f369dd973b278dc08
mayur-v-p <mayurvpawar8@gmail.com>
2025-01-31 00:53:19 +0530

    login_signup ui

--- client/imagify/src/components/Header.jsx
+++ client/imagify/src/components/Header.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
-
+import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+  const { user, setShowLogin } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    if (user) {
+      navigate("/result");
+    } else {
+      setShowLogin(true);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center text-center my-20">
       <div className="text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500">
@@ -18,7 +30,10 @@ const Header = () => {
         delectus nesciunt,
       </p>
 
-      <button className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full">
+      <button
+        onClick={onClickHandler}
+        className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full"
+      >
         Generate Images
         <img className="h-6" src={assets.star_group} alt="" />
       </button>
