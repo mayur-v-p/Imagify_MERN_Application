6a2fe55cc0a843839e8a7a695b275f4394c8b2de
mayur-v-p <mayurvpawar8@gmail.com>
2025-01-31 01:04:33 +0530

    frontend

--- client/imagify/src/components/GenerateButton.jsx
+++ client/imagify/src/components/GenerateButton.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
+const GenerateButton = () => {
+  const { user, setShowLogin } = useContext(AppContext);
+  const navigate = useNavigate();
 
-function GenerateButton() {
+  const onClickHandler = () => {
+    if (user) {
+      navigate("/result");
+    } else {
+      setShowLogin(true);
+    }
+  };
   return (
     <div className="pb-16 text-center">
       <h1
@@ -10,12 +21,15 @@ function GenerateButton() {
       >
         See the magic. Try now
       </h1>
-      <button className="inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 transition-all duration-500">
+      <button
+        onClick={onClickHandler}
+        className="inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 transition-all duration-500"
+      >
         Generate Image
         <img src={assets.star_group} alt="" className="h-6" />
       </button>
     </div>
   );
-}
+};
 
 export default GenerateButton;
